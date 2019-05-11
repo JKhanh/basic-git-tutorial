3b5fbf59cd4dc835d1ce9fe27984c53c0c34e68b
Jonas Khanh <nguyenkhanhcv@gmail.com>
2019-05-11 21:41:17 +0700

    Add file Simple.java

---
+++ Simple.java
@@ -0,0 +1,11 @@
+package Sample;
+
+public class Simple {
+    public static void main(String... args)
+    {
+        System.out.print("Ten 1 ban dep trai: ");
+        System.out.println("Nguyen Quoc Khanh ne!!!!");
+        System.out.println("Minh cung hoc dem nhe cac ban nho.");
+        for(int i=1; i<=10;i++) System.out.println("So " + i);
+    }
+}
